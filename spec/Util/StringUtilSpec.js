bf036ef9a9c53bebe517e178368ce91c63b5d5f9
Mat Gadd <mat@zerifas.com>
2017-04-12 11:56:10 +0100

    Remove unnecessary function from StringUtils

--- spec/Util/StringUtilSpec.js
+++ spec/Util/StringUtilSpec.js
@@ -1,17 +1,13 @@
 describe('String', () => {
   let StringUtil = require('../../lib/Util/StringUtil');
 
-  beforeEach(() => {
-    // Nothing yet
-  });
-
   it('should convert kebab to camel', () => {
     let actual = StringUtil.kebabToCamel('kebab-case-string');
     expect(actual).toEqual('kebabCaseString');
   });
 
-  it('should convert camel to kebab', () => {
-    let actual = StringUtil.camelToKebab('camelCaseString');
-    expect(actual).toEqual('camel-case-string');
+  it('should omit trailing kebab hyphens', () => {
+    let actual = StringUtil.kebabToCamel('-kebab-case-string-');
+    expect(actual).toEqual('kebabCaseString');
   });
 });
