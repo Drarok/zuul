fd1347b7d2736491e2d4c89976c016923bd0a95c
Mat Gadd <mat@zerifas.com>
2017-06-21 13:41:52 +0100

    Test with 10 servers, 30 keys

--- docker.sh
+++ docker.sh
@@ -12,11 +12,9 @@ docker pull $base
 
 # Start a few instances, storing their ids for later
 instances=""
-instances="$instances $(docker run --rm -d $base)"
-instances="$instances $(docker run --rm -d $base)"
-instances="$instances $(docker run --rm -d $base)"
-
-echo "Started $instances"
+for i in {1..10}; do
+    instances="$instances $(docker run --rm -d $base)"
+done
 
 # Use Docker inspect to get the IP addresses of the instances we just started
 servers=$(echo "$instances" | \
@@ -24,27 +22,25 @@ servers=$(echo "$instances" | \
     jq -r '.[0].NetworkSettings.IPAddress');
 
 echo '#!/bin/sh' > enroll.sh
-echo bin/zuul user add user1 --key=\'ssh-rsa abcdef12345678901 user1@client1\' >> enroll.sh
-echo bin/zuul user add user2 --key=\'ssh-rsa abcdef12345678902 user2@client2\' >> enroll.sh
-echo bin/zuul user add user3 --key=\'ssh-rsa abcdef12345678903 user3@client3\' >> enroll.sh
 echo bin/zuul group add developers >> enroll.sh
-echo bin/zuul group add-user developers user1 >> enroll.sh
-echo bin/zuul group add-user developers user2 >> enroll.sh
-
-i=0
-for s in $servers; do
-    echo bin/zuul enroll $s root@$s >> enroll.sh
-    echo bin/zuul server add $s root@$s --group=developers >> enroll.sh
-    if [[ $i -eq 0 ]]; then
-        echo bin/zuul server add-user $s user3 >> enroll.sh
-    fi
-    let i=$i+1
+users=""
+for i in {1..30}; do
+    echo bin/zuul user add user$i --key=\"ssh-rsa abcdef12345678901 user$i@client$i\" >> enroll.sh
+    users="$users user$i"
 done
-echo bin/zuul sync --verbose >> enroll.sh
+
+echo bin/zuul group add-user developers $users >> enroll.sh
+
+for s in $servers; do
+    echo bin/zuul enroll $s root@$s --password=root >> enroll.sh
+    echo bin/zuul server add $s root@$s --group=developers >> enroll.sh
+done
+echo bin/zuul sync >> enroll.sh
 chmod 0755 enroll.sh
 
 docker build -t zerifas/zuul .
 docker run --rm -v $(pwd):/mnt/zuul -it zerifas/zuul sh
+echo
 
 # Clear up the instances we started earlier
 docker rm -f $instances
