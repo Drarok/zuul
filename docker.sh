5cc1cc01cb06e795aa25d106b531d7d289be3fce
Mat Gadd <mat@zerifas.com>
2017-10-30 13:05:11 +0000

    Improved docker test script

--- docker.sh
+++ docker.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Remove all pre-existing zuul data
-rm -rf data
-mkdir -p data/groups
-mkdir -p data/servers
-mkdir -p data/users
-
 # Pull down an image we can SSH to (username and password are root)
 base="rastasheep/ubuntu-sshd"
 docker pull $base
@@ -22,21 +16,23 @@ servers=$(echo "$instances" | \
     jq -r '.[0].NetworkSettings.IPAddress');
 
 echo '#!/bin/sh' > enroll.sh
+echo 'set -e' >> enroll.sh
+echo 'rm -rf data' >> enroll.sh
+
 echo bin/zuul group add developers >> enroll.sh
-users=""
-for i in {1..30}; do
-    echo bin/zuul user add user$i --key=\"ssh-rsa abcdef12345678901 user$i@client$i\" >> enroll.sh
-    users="$users user$i"
+keys=""
+for i in {1..10}; do
+    echo bin/zuul key add user$i --key=\"ssh-rsa abcdef12345678901 user$i@client$i\" >> enroll.sh
+    keys="$keys user$i"
 done
 
-echo bin/zuul group add-user developers $users >> enroll.sh
+echo bin/zuul group add-key developers $keys >> enroll.sh
 
 for s in $servers; do
-    echo bin/zuul enroll $s root@$s --password=root >> enroll.sh
-    echo bin/zuul server add $s root@$s --group=developers >> enroll.sh
+    echo bin/zuul enroll root@$s --password=root --group=developers >> enroll.sh
 done
 echo bin/zuul sync >> enroll.sh
-chmod 0755 enroll.sh
+chmod +x enroll.sh
 
 docker build -t zerifas/zuul .
 docker run --rm -v $(pwd):/mnt/zuul -it zerifas/zuul sh
